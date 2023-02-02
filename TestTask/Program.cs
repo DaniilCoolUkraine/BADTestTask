1487dc0928b6af6531f75189d7818154b411f740
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-02-02 22:03:48 +0200

    added initial project files

---
+++ TestTask/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestTask
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            
+            
+        }
+    }
+}
